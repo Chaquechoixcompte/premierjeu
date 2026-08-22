b150b45e600958a49bf7bce8cb27f86ec0ca7730
rugbyniversaire <rugbyniversaire@gmail.com>
2026-08-22 15:08:43 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -8,7 +8,7 @@
 // pour ça), mais la sécurité réelle se fait via les règles RLS (Row Level
 // Security) dans Supabase, comme pour l'appli famille.
 
-const SUPABASE_URL = "https://TON-PROJET.supabase.co";
-const SUPABASE_ANON_KEY = "TA_CLE_ANON_PUBLIC";
+const SUPABASE_URL = "https://rczjinjvhbwfdeyioawy.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjemppbmp2aGJ3ZmRleWlvYXd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczOTg1ODUsImV4cCI6MjEwMjk3NDU4NX0.oC8g37v8yrJM5yIWQcV7OSCEMU2TE_V1EI2NCemiKqg";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
